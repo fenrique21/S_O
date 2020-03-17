d908a0dcf35ef665e5dda76bfda40dfefe8d7135
fenrique21 <fenrique.21@hotmail.com>
2020-03-16 19:39:01 -0600

    Formulario principal

--- src/Principal.java
+++ src/Principal.java
@@ -16,7 +16,7 @@ public class Principal extends javax.swing.JFrame {
     public Principal() {
         initComponents();
     }
-
+//hola mundo
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
